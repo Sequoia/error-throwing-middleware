7e0f94b25c6a148be488c87a567c50ea1ca4c8ae
Sequoia McDowell <sequoia.mcdowell@gmail.com>
2015-08-10 14:10:57 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Install
+
+```
+npm i -S error-throwing-middleware
+```
+
+# Use
+
+```javascript
+var thrower = require('error-throwing-middleware');
+app.use(thrower);
+```
+Now you randomly get errors! :)
---
+++ index.js
@@ -0,0 +1,31 @@
+var errs = [
+  [ 503, 'Uhoh something went wrong!'],
+  [ 501, 'Oh wow, 501.. that sounds bad :/'],
+  ['ENOENT', 'They went to search for the ENTwives'],
+  ['EACCES', 'We could not afford a second "S" :('],
+  [ 418, 'I\'m a teapot']
+];
+
+var frequency = 0.6; //throw ~60% of time
+
+function shouldThrow(threshold){
+  return Math.random() < threshold;
+}
+
+function pickRandom(ray){
+  return ray[Math.floor(Math.random() * ray.length)];
+}
+
+function makeError(err){
+  var e = new Error(err[1]);
+  e.code = err[0];
+  return e;
+}
+
+module.exports = function(req, res, next){
+  if(shouldThrow(frequency)){
+    next(makeError(pickRandom(errs)));
+  }else{
+    next();
+  }
+};
